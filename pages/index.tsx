9e45320471cafcce016099e070b6e6917f166bf5
Aldycky1 <bagusaldycky@gmail.com>
2021-11-21 19:13:53 +0700

    optimalisasi image di next js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,224 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/profile.jpg" width={200} height={200} alt="profile" />
       {/* styles using module css */}
       <h1 className={styles["title-homepage"]}>Welcome Aldy</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <Image src="/post1.jpg" width={200} height={200} alt="post image 1" />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus
+        dolorum reprehenderit placeat ullam est expedita iusto! Quidem corporis
+        excepturi natus facilis praesentium, enim facere minima unde quod quo
+        amet itaque esse voluptatibus aut ipsam nesciunt vero sed repellendus
+        mollitia eveniet tenetur quas fuga, qui possimus? Ipsum, quibusdam
+        fugit, dolor possimus voluptatum quod animi recusandae sequi vel
+        corrupti eaque eligendi? Porro culpa inventore quisquam officiis aliquam
+        eveniet quia, rerum neque obcaecati omnis. Debitis odit enim nisi unde
+        eum voluptatum. Earum laboriosam eius unde illo consequatur dolorum
+        distinctio id eveniet quia iure iste ullam in harum sint enim, omnis
+        soluta illum.
+      </p>
+      <Image src="/post2.jpg" width={200} height={200} alt="post image 2" />
     </Layout>
   );
 };
